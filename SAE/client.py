63537bf12dcf3ca207224b9545222a6d92b5c795
HERTRICH <william.hertrich@numericable.fr>
2022-11-24 16:33:52 +0100

    SAE update

--- SAE/client.py
+++ SAE/client.py
@@ -1,7 +1,44 @@
 import socket
+import threading
+import sys
 
-client_socket = socket.socket()
-ip_ask = str(input('Destination IP : '))
-port_ask = int(input('Port associé : '))
-client_socket.connect((ip_ask, port_ask))
-ask = str(input('[-] start chatting y/n : '))
+
+def envoi(ask, client_socket):
+    if ask == 'y':
+        while True:
+            message = input('[+] ')
+            client_socket.send(message.encode())
+            if message == 'end':
+                try:
+                    client_socket.close()
+                    break
+                except ConnectionAbortedError:
+                    print("[-] disconnected ...")
+                except KeyboardInterrupt:
+                    print("[!] Keyboard interrupt")
+
+
+def reception(ask, client_socket):
+    if ask == 'y':
+        while True:
+            data = client_socket.recv(1024).decode()
+            print(data)
+
+
+if __name__ == '__main__':
+    sys.tracebacklimit = 0
+    client_socket = socket.socket()
+    ip_ask = str(input('Destination IP : '))
+    port_ask = int(input('Port associé : '))
+    client_socket.connect((ip_ask, port_ask))
+    ask = str(input('[-] start chatting y/n : '))
+
+    if ask == 'n':
+        client_socket.close()
+    elif ask == 'y':
+        task_message = threading.Thread(target=envoi, args=[ask, client_socket])
+        task_reception = threading.Thread(target=reception, args=[ask, client_socket])
+        task_message.start()
+        task_reception.start()
+    else:
+        client_socket.close()
--- SAE/serveur.py
+++ SAE/serveur.py
@@ -1,22 +1,51 @@
 import socket
+import threading
+import sys
+import os
 
-server_socket = socket.socket()
-server_socket.bind(('127.0.0.1', 5000))
-server_socket.listen(1)
-conn, address = server_socket.accept()
-ask = str(input('[-] start chatting y/n : '))
-#reply = str(input('[-]'))
-if ask == 'y':
-    while ask != 'end' or 'stop'or 'arrêt' or 'arret':
-        data = conn.recv(1024).decode()
-        print(data)
-        reply = str(input('[+]'))
+
+def reply(conn, reception):
+    while True:
+        reply = input('[+] ')
         conn.send(reply.encode())
         if reply == 'end':
-            conn.close()
-            break
-    else:
-        print('Deconnection en cours ...')
-        conn.close
-if ask == 'n':
-    conn.close()
+            try:
+                print("[-] L'hôte a décider de se déconnecter ...")
+            except (KeyboardInterrupt):
+                print("[!] Keyboard interrupt")
+            except (ConnectionResetError, EOFError):
+                print("[!] L'hôte s'est déconnecté . .. ...")
+        if reply == 'killServer':
+            try:
+                server_socket.close()
+                break
+            except (ConnectionAbortedError, EOFError):
+                print("[!] L'hôte a décider de tuer la connection serveur sad_4_u ...")
+                
+
+
+def reception(conn,reply):
+        while True:
+            try:
+                data = conn.recv(1024).decode()
+                print(data)
+            except (ConnectionResetError, ConnectionAbortedError, EOFError):
+                print("[!] L'hôte s'est déconnecté . .. ...")
+            if data == 'os':
+                systeme_exploitation = os.name
+                if systeme_exploitation == 'nt':
+
+
+if __name__ == '__main__':
+    sys.tracebacklimit = 0
+    server_socket = socket.socket()
+    server_socket.bind(('0.0.0.0', 6969))  # 127.0.0.1
+    server_socket.listen(1)
+    conn, address = server_socket.accept()
+    try:
+        task_reply = threading.Thread(target=reply, args=[conn,reply])
+        task_reply.start()
+        task_reception = threading.Thread(target=reception, args=[conn,reception])
+        task_reception.start()
+    except (ConnectionResetError, ConnectionAbortedError, EOFError):
+        print("[!] L'hôte s'est déconnecté . .. ...")
