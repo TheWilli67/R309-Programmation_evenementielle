106fa284285f477195f303bc12c3496f39d67b2b
HERTRICH <william.hertrich@numericable.fr>
2022-12-23 09:56:02 +0100

    update

--- SAE/test.py
+++ SAE/test.py
@@ -5,8 +5,7 @@ liste_port = ['6969']
 liste_al = []
 
 with open("./test.csv") as file:
-  csvreader = csv.reader(file, delimiter=';')
-  for row in csvreader:
+  csvreader = csv.reader(file)
+  csvr = csvreader.split(";")[1]
+  for row in csvr:
     print(row)
-    
-print(liste_al)
